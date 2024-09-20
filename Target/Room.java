412eb3acfb4eff6f87b3e0a099e12a48af9c75a3
tartar008 <127741947+tartar008@users.noreply.github.com>
2024-09-20 04:58:05 +0000

    commit

--- Target/Room.java
+++ Target/Room.java
@@ -3,5 +3,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Room {
+    private String roomNumber;
+    private String type;
+    private double price;
+
+    public Room(String roomNumber, String type, double price) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.price = price;
+    }
+
+
    
 }
