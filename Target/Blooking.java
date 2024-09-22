13b897d07ec8493c8431cf5b49be80630f34aed5
Nongkreem <kareemamukem@gmail.com>
2024-09-22 16:13:22 +0700

    Create booking

--- Target/Blooking.java
+++ Target/Blooking.java
@@ -1,3 +1,11 @@
+import java.time.LocalDate;
+
 public class Blooking {
-    
+    private String bookingID;
+    private Customer customer;
+    private Room room;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private double totalPrice;
+
 }
