71946c4e55e9d60b364730daa7f657e8c9169341
Tar <tartar0081@gmail.com>
2024-09-23 04:47:52 +0700

    commit

---
+++ Components/Debug/Target/Customer.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Customer extends Person{
+    HashMap<Integer,String> guest = new HashMap<>();
+
+    public Customer(String firstName, String lastName, String phoneNumber, String email){
+        super(firstName, lastName, phoneNumber, email);
+    }
+
+    public void setGuest(int roomNumber , String guestName){
+        guest.put(roomNumber, guestName); //roomNumber เป็น key, guestName เป็น value
+    }
+
+    // Method สำหรับดึงข้อมูล guest ทั้งหมด
+    public HashMap<Integer, String> getGuests() {
+        return guest;
+    }
+    // Method สำหรับดึงข้อมูล guest โดยใช้หมายเลขห้องพัก
+    public String getGuestByRoomNumber(int roomNumber) {
+        return guest.get(roomNumber);
+    }
+
+    public void customerInfo(){
+        super.printInfo();
+    }
+
+}
--- Tar/Main.java
+++ Tar/Main.java
@@ -1,3 +1,4 @@
+import java.lang.classfile.instruction.StackInstruction;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
